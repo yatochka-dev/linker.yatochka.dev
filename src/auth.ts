570b227fbfa33c463052ce2240f6058f1f90a448
Yatochka <philipchef13@gmail.com>
2024-06-26 19:03:35 +0300

    feat(auth): add google auth

--- src/auth.ts
+++ src/auth.ts
@@ -4,14 +4,21 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 import Discord from '@auth/sveltekit/providers/discord';
-import { AUTH_DISCORD_ID, AUTH_DISCORD_SECRET } from '$env/static/private';
+import Google from '@auth/sveltekit/providers/google';
+import { AUTH_DISCORD_ID, AUTH_DISCORD_SECRET,AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET  } from '$env/static/private';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
 	adapter: PrismaAdapter(prisma),
 	providers: [
 		Discord({
 			clientId: AUTH_DISCORD_ID,
-			clientSecret: AUTH_DISCORD_SECRET
+			clientSecret: AUTH_DISCORD_SECRET,
+			allowDangerousEmailAccountLinking: true,
+		}),
+		Google({
+			clientId: AUTH_GOOGLE_ID,
+			clientSecret: AUTH_GOOGLE_SECRET,
+			allowDangerousEmailAccountLinking: true
 		})
 	],
 	// debug: true,
